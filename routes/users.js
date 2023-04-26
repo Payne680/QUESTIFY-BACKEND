e7c3aac980a3927adc866585eeaad73f7197c681
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2023-04-26 11:24:12 +0100

    pull and merge conflicts

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@ var express = require("express");
 const UserController = require("../modules/user/userController");
 var router = express.Router();
 
-
 const userController = new UserController();
 
 /* GET users listing. */
-     router.get('/', userController.getAllUsers.bind(userController))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-
+router.get("/", userController.getAllUsers.bind(userController));
+router.get("/:id", userController.getOneUser.bind(userController));
+router.post("/", userController.createOneUser.bind(userController));
+router.put("/:id", userController.patchOneUser.bind(userController));
+router.delete("/:id", userController.deleteOneUser.bind(userController));
 module.exports = router;
